f5ccc6327726f455b545dbef56ab24212856f91d
Sajad Yoosuf <sajadyoosuf24@gmail.com>
2026-01-07 18:24:14 +0530

    onky the true changed

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, //Root widget
+      debugShowCheckedModeBanner: true, //Root widget
       home: Scaffold(
         body: Column(
 
