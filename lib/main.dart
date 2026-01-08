f4e3f5b4c9d3bf686003a42023d9bca3b49d30b1
Akhila <akhilapg637@gmail.com>
2026-01-08 17:39:16 +0530

    edited text and button

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, //Root widget
+      //Root widget
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Column(
-
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 100,),
-            Align(alignment: Alignment.topCenter,child: Text('What do you thing you will mostly use?')),
-            Align(alignment: Alignment.center,
+            SizedBox(height: 75),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                'What do you thing you will\n mostly use?',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 50),
+            Align(
+              alignment: Alignment.center,
               child: Text(
                 'Top on all that apply.This will help us\n customise your home page',
                 textAlign: TextAlign.center,
@@ -26,9 +35,35 @@ class MyApp extends StatelessWidget {
                 overflow: TextOverflow.ellipsis,
               ),
             ),
+            SizedBox(height: 300),
+            Positioned(
+              bottom: 20,
+              left: 0,
+              right: 0,
+
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    //print('Button clicked');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[700],
+                    foregroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+
+                  child: Text(
+                    'Next',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
-
       ),
     );
   }
