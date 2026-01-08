3b92c524c3ab41982420adb6309fe5dc39f6a477
Akhila <akhilapg637@gmail.com>
2026-01-08 18:13:16 +0530

    edited text and button code

--- lib/main.dart
+++ lib/main.dart
@@ -10,61 +10,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //Root widget
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 75),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Text(
-                'What do you thing you will\n mostly use?',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                textAlign: TextAlign.center,
-              ),
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 75),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              'What do you thing you will\n mostly use?',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              textAlign: TextAlign.center,
             ),
-            SizedBox(height: 50),
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                'Top on all that apply.This will help us\n customise your home page',
-                textAlign: TextAlign.center,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
+          ),
+          SizedBox(height: 50),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Top on all that apply.This will help us\n customise your home page',
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
             ),
-            SizedBox(height: 300),
-            Positioned(
-              bottom: 20,
-              left: 0,
-              right: 0,
+          ),
+          SizedBox(height: 300),
+          Positioned(
+            bottom: 20,
+            left: 0,
+            right: 0,
 
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    //print('Button clicked');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red[700],
-                    foregroundColor: Colors.black,
-                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  //print('Button clicked');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red[700],
+                  foregroundColor: Colors.black,
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
                   ),
+                ),
 
-                  child: Text(
-                    'Next',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
+                child: Text(
+                  'Next',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+    ),
     );
   }
 }
